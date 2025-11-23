dab609a906a55d8f65f975ef7180ba43f3a2908b
MaSeungHyun <tpdlqj0514@gmail.com>
2025-11-23 23:27:03 +0900

    feat:  구현된 페이지에 대해 라우팅을 위한 Stack layout 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,42 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DarkTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen
+          name="(root)/index"
+          options={{
+            headerShown: false,
+            contentStyle: { backgroundColor: Colors.dark.background.primary },
+          }}
+        />
+        <Stack.Screen
+          name="webview/index"
+          options={{
+            headerShown: false,
+            contentStyle: { backgroundColor: Colors.dark.background.primary },
+            animation: "none",
+          }}
+        />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </ThemeProvider>
   );
 }
